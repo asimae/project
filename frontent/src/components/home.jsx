e7950d14deda015255fe5baffa90732cd8f50f71
asimae <165981302+asimae@users.noreply.github.com>
2024-04-24 13:38:11 +0530

    Add files via upload

---
+++ frontent/src/components/home.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+  const dispatch = useDispatch()
+  const navigator = useNavigate()
+  const logout = () => {
+    dispatch({ type: 'SET_USER', payload: 'user not found' }); 
+    navigator('/login')
+  }
+  const user = useSelector((state) => state.user.user);
+
+  return (
+    <di>
+      <div></div>
+
+
+
+
+    </di>
+
+
+
+  );
+};
+
+export default Home;
